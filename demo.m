cc33d82d5df7d5d9b72f23cc264f57ec7a9d830b
annasli378 <86146614+annasli378@users.noreply.github.com>
2023-05-15 19:41:57 +0200

    adjustments

--- demo.m
+++ demo.m
@@ -1,23 +1,21 @@
 clear all, close all
 %% Read mask
-MASK  = load("CALE_OBRAZKI\Masks\19_Mask.mat");
+MASK  = load("CALE_OBRAZKI\Masks\20_Mask.mat");
 mask = MASK.ABCD;
 
 
 %% Better mask:
 maska_membrana = GetMask(mask);
-% figure, imshow(maska_membrana)
-
-    
+figure, imshow(maska_membrana)
 
 %% Get regions:
 [mask_attributes_struct, num_blobs] = GetBlobs(maska_membrana);
 
 %% Get widths:
-step = 50;
+step = 20;
 start = 10;
 
-widths = GetWidths(start,step, mask_attributes_struct, num_blobs, false);
+widths = GetWidths3(start,step, mask_attributes_struct, num_blobs, false );
 
 %%  pix -> nm
 nm_pix = 0.15;
@@ -25,8 +23,9 @@ pow = 3000;
 for i = 1: length(widths)
     widths_nm(i) = widths(i) /  nm_pix ;
 end
-%% Harmonic average of 20 random values:
-r = randperm(length(widths_nm),20);
+%% Harmonic average of N random values:
+N = 20;
+r = randperm(length(widths_nm),N);
 
 h_mean = harmmean(widths_nm)
 h_mean_r20 = harmmean(widths_nm(r))
